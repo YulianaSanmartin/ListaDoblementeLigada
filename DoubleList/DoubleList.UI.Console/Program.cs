80d338473a2dbe383d6379ed92f579939fe88704
YulianaSanmartin <yulianasanmartin1124627@correo.itm.edu.co>
2025-05-23 18:16:04 -0500

    commint

---
+++ DoubleList/DoubleList.UI.Console/Program.cs
@@ -0,0 +1,88 @@
+﻿using DoubleList;
+
+var list = new DoublyLinkedList<string>();
+var opc = "0";
+
+    do
+    {
+        opc = ShowMenu();
+        Console.WriteLine();
+        switch (opc)
+        {
+            case "1":
+                Console.Write("Enter value to add: ");
+                var value = Console.ReadLine();
+                if (value != null) list.Add(value);
+                break;
+
+            case "2":
+                Console.WriteLine("List (forward):");
+                Console.WriteLine(list.GetForward());
+                break;
+
+            case "3":
+                Console.WriteLine("List (backward):");
+                Console.WriteLine(list.GetBackward());
+                break;
+
+            case "4":
+                list.SortDescending();
+                Console.WriteLine("List sorted in descending order.");
+                break;
+
+            case "5":
+                var modes = list.GetModes();
+                Console.WriteLine("Mode(s): " + string.Join(", ", modes));
+                break;
+
+            case "6":
+                Console.WriteLine("Graph of occurrences:");
+                Console.WriteLine(list.GetGraph());
+                break;
+
+            case "7":
+                Console.Write("Enter value to check: ");
+                var existsValue = Console.ReadLine();
+                if (existsValue != null)
+                    Console.WriteLine(list.Exists(existsValue) ? "Exists" : "Does not exist");
+                break;
+
+            case "8":
+                Console.Write("Enter value to remove one occurrence: ");
+                var removeOne = Console.ReadLine();
+                if (removeOne != null) list.RemoveOne(removeOne);
+                break;
+
+            case "9":
+                Console.Write("Enter value to remove all occurrences: ");
+                var removeAll = Console.ReadLine();
+                if (removeAll != null) list.RemoveAll(removeAll);
+                break;
+
+            case "0":
+                Console.WriteLine("Exiting...");
+                break;
+
+            default:
+                Console.WriteLine("Invalid option.");
+                break;
+        }
+        Console.WriteLine();
+    } while (opc != "0");
+
+    static string ShowMenu()
+{
+    Console.WriteLine("MENU");
+    Console.WriteLine("1. Add (ascending order)");
+    Console.WriteLine("2. Show forward");
+    Console.WriteLine("3. Show backward");
+    Console.WriteLine("4. Sort descending");
+    Console.WriteLine("5. Show mode(s)");
+    Console.WriteLine("6. Show graph");
+    Console.WriteLine("7. Check if value exists");
+    Console.WriteLine("8. Remove one occurrence");
+    Console.WriteLine("9. Remove all occurrences");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine() ?? "0";
+}
---
+++ DoubleList/DoubleList/DoubleLinkedList.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList;
+public class DoublyLinkedList<T> where T : IComparable<T>
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public void Add(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)
+        {
+            _head = _tail = newNode;
+            return;
+        }
+
+        var current = _head;
+        while (current != null && current.Data.CompareTo(data) < 0)
+        {
+            current = current.Next;
+        }
+
+        if (current == null)
+        {
+            _tail!.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+        else if (current == _head)
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+        else
+        {
+            newNode.Prev = current.Prev;
+            newNode.Next = current;
+            current.Prev!.Next = newNode;
+            current.Prev = newNode;
+        }
+    }
+
+    public string GetForward()
+    {
+        var current = _head;
+        var result = new List<string>();
+        while (current != null)
+        {
+            result.Add(current.Data!.ToString()!);
+            current = current.Next;
+        }
+        return string.Join(" <=> ", result);
+    }
+
+    public string GetBackward()
+    {
+        var current = _tail;
+        var result = new List<string>();
+        while (current != null)
+        {
+            result.Add(current.Data!.ToString()!);
+            current = current.Prev;
+        }
+        return string.Join(" <=> ", result);
+    }
+
+    public void SortDescending()
+    {
+        var items = new List<T>();
+        var current = _head;
+        while (current != null)
+        {
+            items.Add(current.Data);
+            current = current.Next;
+        }
+
+        items.Sort();
+        items.Reverse();
+
+        _head = _tail = null;
+        foreach (var item in items)
+        {
+            var newNode = new DoubleNode<T>(item);
+            if (_head == null)
+            {
+                _head = _tail = newNode;
+            }
+            else
+            {
+                _tail!.Next = newNode;
+                newNode.Prev = _tail;
+                _tail = newNode;
+            }
+        }
+    }
+
+    public List<T> GetModes()
+    {
+        var frequency = new Dictionary<T, int>();
+        var current = _head;
+        while (current != null)
+        {
+            if (!frequency.ContainsKey(current.Data))
+                frequency[current.Data] = 0;
+            frequency[current.Data]++;
+            current = current.Next;
+        }
+
+        int maxFreq = frequency.Values.Max();
+        return frequency.Where(kvp => kvp.Value == maxFreq).Select(kvp => kvp.Key).ToList();
+    }
+
+    public string GetGraph()
+    {
+        var frequency = new Dictionary<T, int>();
+        var current = _head;
+        while (current != null)
+        {
+            if (!frequency.ContainsKey(current.Data))
+                frequency[current.Data] = 0;
+            frequency[current.Data]++;
+            current = current.Next;
+        }
+
+        return string.Join("\n", frequency.Select(kvp => $"{kvp.Key} {new string('*', kvp.Value)}"));
+    }
+
+    public bool Exists(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+                return true;
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void RemoveOne(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                    current.Prev.Next = current.Next;
+                else
+                    _head = current.Next;
+
+                if (current.Next != null)
+                    current.Next.Prev = current.Prev;
+                else
+                    _tail = current.Prev;
+                return;
+            }
+            current = current.Next;
+        }
+    }
+    public void RemoveAll(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            var next = current.Next;
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                    current.Prev.Next = current.Next;
+                else
+                    _head = current.Next;
+
+                if (current.Next != null)
+                    current.Next.Prev = current.Prev;
+                else
+                    _tail = current.Prev;
+            }
+            current = next;
+        }
+    }
+}
